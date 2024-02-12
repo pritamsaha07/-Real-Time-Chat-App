e3d3c9801abd60ca8129133a0c90c0025ef32f76
pritamsaha07 <sahapritam2002@gmail.com>
2024-02-12 23:02:16 +0530

    first commit

---
+++ src/main/java/com/pritam/chat/chat/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.pritam.chat.chat;
+
+
+import java.awt.TrayIcon.MessageType;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
